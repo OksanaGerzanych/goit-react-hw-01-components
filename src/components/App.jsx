fc941244d271828507b3e58248f9b987b14bf910
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2023-02-15 21:22:13 +0200

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,18 @@ import { Profile } from './Profile/Profile';
 import user from './Profile/user.json'
 import { Statistics } from './Statistics/Statistics';
 import data from './Statistics/data.json'
+import { FriendList } from './FriendList/FriendList';
+import friends from './FriendList/friends.json'
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json'
 
 export const App = () => {
   return (
     <div>
       <Profile propsUser={user} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
